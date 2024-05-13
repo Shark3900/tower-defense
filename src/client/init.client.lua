c35ffd9a941cc92667792a936c5c9af96b2df26d
Shark3900 <justin.sargent1@gmail.com>
2024-05-12 21:28:14 -0400

    Init Player - Wrote initial PlayerService and Player class - Added Health to GameManager - Added cash counter UI and update function to client
     Changes to be committed:
    	modified:   src/client/init.client.lua
    	modified:   src/server/GameManager/EnemyService/init.luau
    	modified:   src/server/GameManager/PlayerService/Player.luau
    	modified:   src/server/GameManager/PlayerService/init.luau
    	modified:   src/server/GameManager/init.luau

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,9 @@
-print("Hello world, from client!")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local cashCounter = player:WaitForChild("PlayerGui"):WaitForChild("PlayerHUD").CashCounter
+
+local function updateCash()
+    cashCounter.Counter.Text = player:GetAttribute("Cash")
+end
+
+player:GetAttributeChangedSignal("Cash"):Connect(updateCash)
