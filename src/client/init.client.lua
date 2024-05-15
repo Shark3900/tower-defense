8552bd5072a84ffa2dd2b021645c96f6102a46bf
Shark3900 <justin.sargent1@gmail.com>
2024-05-15 12:13:58 -0400

    Changed cash increment and init player controls - Changed Cash from on enemy death to on damage dealt - Added tower selection to player controls
    Changes to be committed:
    	modified:   src/client/init.client.lua
    	modified:   src/server/GameManager/EnemyService/Enemy/init.luau
    	modified:   src/server/GameManager/EnemyService/init.luau
    	modified:   src/server/GameManager/PlayerService/Player.luau
    	modified:   src/server/GameManager/PlayerService/init.luau
    	modified:   src/server/GameManager/TowerService/Spawner/Unit/init.luau
    	modified:   src/server/GameManager/TowerService/Tower/init.luau
    	modified:   src/server/GameManager/TowerService/init.luau
    	modified:   src/server/GameManager/init.luau

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,9 +1,18 @@
+--#region Variables
+local ContextActionService = game:GetService("ContextActionService")
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local playerHUD = player:WaitForChild("PlayerGui"):WaitForChild("PlayerHUD")
-local cashCounter = playerHUD.CashGUI.Counter
-local healthCounter = playerHUD.HealthGUI.Counter
+local cashCounter = playerHUD:WaitForChild("CashGUI").Counter
+local healthCounter = playerHUD:WaitForChild("HealthGUI").Counter
+local towerGUI = playerHUD:WaitForChild("Towers")
+local towers = {"Basic", "Sniper", "Tesla", "Mortar"}
+local selectedTower
+--#endregion
+--TODO: Not hardcode this
+towerGUI.TowerOne.TextButton.Text, towerGUI.TowerTwo.TextButton.Text, towerGUI.TowerThree.TextButton.Text, towerGUI.TowerFour.TextButton.Text = towers[1], towers[2], towers[3], towers[4]
 
+--#region Functions
 local function updateCash()
     cashCounter.Text = player:GetAttribute("Cash")
 end
@@ -12,5 +21,31 @@ local function updateHealth()
     healthCounter.Text = workspace:GetAttribute("Health")
 end
 
+local function selectTower(actionName, inputState, _inputObject)
+    if actionName == "SelectOne" and inputState == Enum.UserInputState.Begin then
+        print(`Selected {towers[1]}`)
+        selectedTower = towers[1]
+    elseif actionName == "SelectTwo" and inputState == Enum.UserInputState.Begin then
+        print(`Selected {towers[2]}`)
+        selectedTower = towers[2]
+    elseif actionName == "SelectThree" and inputState == Enum.UserInputState.Begin then
+        print(`Selected {towers[3]}`)
+        selectedTower = towers[3]
+    elseif actionName == "SelectFour" and inputState == Enum.UserInputState.Begin then
+        print(`Selected {towers[4]}`)
+        selectedTower = towers[4]
+    end
+end
+--#endregion
+
+--#region Listeners
 player:GetAttributeChangedSignal("Cash"):Connect(updateCash)
-workspace:GetAttributeChangedSignal("Health"):Connect(updateHealth)
+workspace:GetAttributeChangedSignal("Health"):Connect(updateHealth) --TODO: Move from ScreenGUI to End part?
+player.CharacterAdded:Connect(function()
+    print("Character added")
+    ContextActionService:BindAction("SelectOne", selectTower, true, Enum.KeyCode.One)
+    ContextActionService:BindAction("SelectTwo", selectTower, true, Enum.KeyCode.Two)
+    ContextActionService:BindAction("SelectThree", selectTower, true, Enum.KeyCode.Three)
+    ContextActionService:BindAction("SelectFour", selectTower, true, Enum.KeyCode.Four)
+end)
+--#endregion
